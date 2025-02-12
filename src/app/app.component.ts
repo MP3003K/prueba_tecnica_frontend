becdd805c4ffe2e3c208aa9cc7c9054f09fb47b6
MP3003K <mijailpalomino@upeu.edu.pe>
2025-02-11 23:34:19 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule], // Incluye HttpClientModule en los imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prueba_tecnica_frontend';
+  breeds: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('https://dogapi.dog/api/v2/breeds').subscribe(response => {
+      this.breeds = response.data;
+    });
+  }
 }
